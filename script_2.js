ed3b0e8a9b81739c7203d731b65e3cf81383efaa
samuelradko <samuelradko@gmail.com>
2022-11-11 01:14:51 +0200

    upd

--- script_2.js
+++ script_2.js
@@ -1,9 +1,9 @@
 // lesson 27 tasks
-// Task 1 + 3 - Create an input and a button, when the button is pressed, calculate how much is the input value + 10. (I used the /, I could not figure out how to do it with +)
+// Task 1 + 3 - Create an input and a button, when the button is pressed, calculate how much is the input value + 10. 
 
 
 function pop() {
-    let num1 = prompt("Please enter a number and I will divide it by 10");
+    let num1 = prompt("Please enter a number and I will add it up by 10");
     if (Number(num1) > 0) {
         document.getElementById("noice").innerHTML =
           `the answer is  ${Number(num1) + 10}`
