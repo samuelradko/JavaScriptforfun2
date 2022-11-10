cd1758ef169094cb34a5c4f01da573a88f656fa5
samuelradko <samuelradko@gmail.com>
2022-11-11 01:00:35 +0200

    upd

--- script_2.js
+++ script_2.js
@@ -4,7 +4,7 @@
 
 function pop() {
     let num1 = prompt("Please enter a number and I will divide it by 10");
-    if (Number(num1 + 10) > 0) {
+    if (Number(num1) > 0) {
         document.getElementById("noice").innerHTML =
             `The Answer is ` + num1 / 10;
     } else {
